c80d4574c1a69a22353a82acc688ace4b229564b
ShpilevskyAndrei <arproject_developer@users.noreply.github.com>
2023-03-29 23:11:36 +0500

    refactoring

--- src/app/modules/overall/components/header/enums/nav-items.ts
+++ src/app/modules/overall/components/header/enums/nav-items.ts
@@ -1,10 +1,9 @@
 import { INavItem } from '../interfaces/nav-item.interface';
 
 export const NavItems: INavItem[] = [
-  { title: 'Скидки', path: '/discounts' },
-  { title: 'nav-1', path: '/nav-1' },
-  { title: 'nav-2', path: '/nav-2' },
-  { title: 'nav-3', path: '/nav-3' },
-  { title: 'nav-4', path: '/nav-4' },
-  { title: 'nav-5', path: '/nav-5' },
+  { title: 'Все skidki', path: '/discounts' },
+  { title: 'О проекте', path: '/nav-1' },
+  { title: 'Пользователям', path: '/nav-2' },
+  { title: 'Компаниям-клиентам', path: '/nav-3' },
+  { title: 'Компаниям-партнерам', path: '/nav-4' },
 ];
